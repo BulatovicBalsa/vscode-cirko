3c93b90977adac8840f35f8b7b926a26e8d0473d
BulatovicBalsa <baki.b.2k02@gmail.com>
2026-07-30 23:12:42 +0200

    feat: add convertSelection command to convert selected text using cirko.exe

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,52 @@
+import path from 'path';
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const cirko_path = path.join(context.extensionPath, 'bin', 'cirko.exe');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-cirko" is now active!');
+	const disposable = vscode.commands.registerCommand('vscode-cirko.convertSelection', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('No active editor found.');
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-cirko.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-cirko!');
+		const selection = editor.selection;
+		const selectedText = editor.document.getText(selection);
+
+		if (!selectedText) {
+			vscode.window.showInformationMessage('No text selected.');
+			return;
+		}
+
+		// we need to send the selected text to the stdin of the cirko.exe process and get the output from its stdout
+		const { spawn } = require('child_process');
+		const cirkoProcess = spawn(cirko_path, [], { shell: true });
+
+		let output = '';
+		cirkoProcess.stdout.on('data', (data: Buffer) => {
+			output += data.toString();
+		});
+
+		cirkoProcess.stderr.on('data', (data: Buffer) => {
+			vscode.window.showErrorMessage(`Error: ${data.toString()}`);
+		});
+
+		cirkoProcess.on('close', (code: number) => {
+			if (code === 0) {
+				editor.edit(editBuilder => {
+					editBuilder.replace(selection, output);
+				});
+			} else {
+				vscode.window.showErrorMessage(`cirko.exe exited with code ${code}`);
+			}
+		});
+
+		cirkoProcess.stdin.write(selectedText);
+		cirkoProcess.stdin.end();
+
 	});
 
 	context.subscriptions.push(disposable);
